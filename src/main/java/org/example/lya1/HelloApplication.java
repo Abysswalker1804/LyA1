d900ef11f3be74938aa8abeec9a90cb5cf1afd6b
Tadeo Palma <tadeo.palavi@gmail.com>
2025-03-12 20:50:47 -0600

    Actualización 12/03/2025

--- src/main/java/org/example/lya1/HelloApplication.java
+++ src/main/java/org/example/lya1/HelloApplication.java
@@ -3,18 +3,45 @@ package org.example.lya1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 public class HelloApplication extends Application {
+    Scene escena;
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage){
+        CrearUI();
+        stage.setTitle("Compilador");
+        stage.setScene(escena);
         stage.show();
+    }
+    private void CrearUI(){
+        TextArea txa_editor=new TextArea();
+
+        Panel pnl_principal=new Panel();
+        //MenuItems
+        MenuItem mit_guardar=new MenuItem("Guardar");
+        MenuItem mit_abrir=new MenuItem("Abrir");
+        MenuItem mit_buscar=new MenuItem("Buscar");
+        MenuItem mit_buscar_reemplazar=new MenuItem("Buscar y Reemplazar");
+        //Menus
+        Menu men_archivo=new Menu("Archivo");
+        men_archivo.getItems().addAll(mit_guardar,mit_abrir);
+        Menu men_editar=new Menu("Editar");
+        men_editar.getItems().addAll(mit_buscar,mit_buscar_reemplazar);
+        //MenuBar
+        MenuBar mbr_principal=new MenuBar();
+        mbr_principal.getMenus().addAll(men_archivo,men_editar);
+
+        pnl_principal.setHeading(mbr_principal);
+        pnl_principal.setBody(txa_editor);
+
+
+        escena=new Scene(pnl_principal,600,600);
     }
 
     public static void main(String[] args) {
